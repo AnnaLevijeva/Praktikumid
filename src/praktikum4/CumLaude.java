1d373fb6714101bb7da90d05b32cde760c7a3410
Anna Levijeva <alevijev@klass2-13-l.intra.itcollege.ee>
2016-09-09 09:25:24 +0300

    test commit

--- src/praktikum4/CumLaude.java
+++ src/praktikum4/CumLaude.java
@@ -4,20 +4,54 @@ import lib.TextIO;
 public class CumLaude {
 	
 	public static void main(String[] args) {
-		while (true) {
 			
-			System.out.print("Sinu keskhind: ");
-			double hind = TextIO.getlnDouble();
+			System.out.print("Sinu lõputöö hind: ");
+			double loputoo = TextIO.getDouble();
+			System.out.print("Sinu kaalatud keskhind: ");
+			double keskmine = TextIO.getDouble();
+			
+			double hind;
+			
+			if (loputoo >= 0 && loputoo <= 5 && keskmine >= 0 && keskmine <= 5) {
+				hind = (loputoo + keskmine)/2;
+				System.out.println("Sinu keskmine hind on " + hind);
+			} else {
+				System.out.println("Vigane sisustus");
+				return;
+			}
+			
+			// System.out.println(hind);
 			
 			if (hind >= 4.5 && hind <= 5) {
 				System.out.println("Jah saad cum laude diplomile!");
-				break;
 			} else if (hind >= 0 && hind < 4.5) {
 				System.out.println("Ei saa");
-				break;
 			} else {
 				System.out.println("Vale! Proovi uuesti");
-			}
-		}
+				return;
+			} 	
 	}
 }
+	
+	
+//	public static double ret()  {
+//		
+//		boolean ask = true;
+//		double loputoo = 0;
+//		double keskmine = 0;
+//		
+//		while (ask) {
+//			System.out.print("Sinu lõputöö hind: ");
+//			loputoo = TextIO.getDouble();
+//			System.out.print("Sinu kaalatud keskhind: ");
+//			keskmine = TextIO.getDouble();
+//			
+//			if (loputoo >= 0 && loputoo <= 5 && keskmine >= 0 && keskmine <= 5) {
+//				ask = false;
+//			} else {
+//				ask = true;
+//			}
+//		}
+//		return (loputoo + keskmine)/2;
+//	}
+//}
---
+++ src/praktikum4/VanusteVahe.java
@@ -0,0 +1,24 @@
+package praktikum4;
+
+import lib.TextIO;
+
+public class VanusteVahe {
+	
+	public static void main(String[] args) {
+		
+		System.out.print("Sisesta esimene vanus: ");
+		int age1 = TextIO.getInt();
+		System.out.print("Sisesta teine vanus: ");
+		int age2 = TextIO.getInt();
+		int vahe = Math.abs(age1 - age2);
+		
+		if (vahe > 5 && vahe < 10) {
+			System.out.println("Siin on midagi krõbedat");
+		} else if (vahe > 10) {
+			System.out.println("Siin on midagi veel krõbedamat");
+		} else {
+			System.out.println("Sobib.");
+		}
+	}
+
+}
