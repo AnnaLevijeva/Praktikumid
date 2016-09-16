e0abecf9238630e7a8edbb508fe43bdca1ae7996
annalevijeva <alevijev@klass2-08-l.intra.itcollege.ee>
2016-09-16 09:46:41 +0300

    tabelid

--- src/praktikum5/Tabel2.java
+++ src/praktikum5/Tabel2.java
@@ -1,21 +1,25 @@
 package praktikum5;
 
-public class Tabel {
+public class Tabel2 {
 
 	public static void main(String[] args) {
 
 		int tabeliSuurus = 7;
-
+		
+		for (int n = 0; n < tabeliSuurus; n++) {
+			System.out.print("--");
+		}
+		System.out.println("");
+		
 		for (int j = 0; j < tabeliSuurus; j++) {
 
 			System.out.print("| ");
 			for (int i = 0; i < tabeliSuurus; i++) {
-				if (i == j) {
-					System.out.print("1 ");
+				if (i == j || i+j == tabeliSuurus-1) {
+					System.out.print("X ");
 				} else {
 					System.out.print("0 ");
 				}
-				System.out.print("0 ");
 			}
 			System.out.print("|");
 			System.out.println("");
