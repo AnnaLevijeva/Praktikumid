0899b0f344fdc6c2a14fbd72acea785101691b46
annalevijeva <annalevijeva@github.com>
2016-10-07 09:02:23 +0300

    massiivid

--- src/praktikum1/HighLow.java
+++ src/praktikum1/HighLow.java
@@ -3,7 +3,6 @@ package praktikum1;
 import lib.TextIO;
 import lib.Card;
 import lib.Deck;
-import lib.TextIO;
 
 /**
  * This program lets the user play HighLow, a simple card game 
--- src/praktikum7/Massiivid.java
+++ src/praktikum7/Massiivid.java
@@ -4,35 +4,57 @@ import java.util.ArrayList;
 import java.util.Random;
 import lib.TextIO;
 
-
 public class Massiivid {
-	
-//	Kirjutada liisu heitmise programm. Programm kysib 10 nime, valib neist suvalise nime 
-//	(randomiga) ja trükib välja. 
-//	Täiendus: Küsida esimese asjana inimeste arv.
-//
-//	Kirjutada programm, mis küsib kasutajalt 10 arvu ning trükib nad seejärel 
-//	vastupidises järjekorras ekraanile.
-	
-		public static void main(String[] args) {
-			
+
+	public static void main(String[] args) {
+
+	} // main
+
+	public static int sum(int[] list) {
+		int ret = 0;
+		for (int i = 0; i < list.length; i++) {
+			ret += list[i];
+		}
+		return ret;
+	}
+
+	public static void arrayFromLast(int listSize) {
+
+		int[] numbers = new int[listSize];
+
+		System.out.println("Enter " + numbers.length + " numbers: ");
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.print("\t" + (i + 1) + ". ");
+			int number = TextIO.getlnInt();
+			numbers[i] = number;
+		}
+
+		System.out.print("You entered (from last to first): \n\t");
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.print(numbers[numbers.length - i - 1] + ", ");
+		}
+	}
+
+	public static String randomName() {
 		Random rand = new Random();
-		
+
 		System.out.print("Count of people: ");
 		int listSize = TextIO.getInt();
 		ArrayList<String> nameList = new ArrayList<String>();
-		
+
 		System.out.println("Enter " + listSize + " names.");
 		for (int i = 0; i < listSize; i++) {
 			System.out.print("\t" + (i + 1) + ". ");
 			String name = TextIO.getlnWord();
 			nameList.add(name);
-		} // for -- declaring nameList array
-		
-		System.out.println("Name list: " + nameList);
-		
+		} // for -- adding names in nameList array
+
+		System.out.println("Names: " + nameList);
+
 		int randomInt = rand.nextInt(6) + 1;
-		//System.out.println(randomInt);
+		// System.out.println(randomInt);
+		System.out.println();
 		System.out.println("Random computer's choose: " + nameList.get(randomInt));
-	} // main
+		return nameList.get(randomInt);
+	} // randomName()
 }
