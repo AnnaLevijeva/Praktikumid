90cb05e4131987cd7999e3c474defb93523d1a41
annalevijeva <annalevijeva@github.com>
2016-10-14 09:11:59 +0300

    sch

---
+++ src/praktikum9/Palindrome.java
@@ -0,0 +1,18 @@
+package praktikum9;
+
+public class Palindrome {
+	
+	public static void main(String[] args) {
+//		irjutada programm, mis küsib kasutajalt sõna ja kontrollib, kas see on edaspidi ja tagurpidi lugedes sama (palindroom).
+//		Näited sellistest sõnadest:
+//
+//		udu
+//		kook
+//		meem
+//		kuulilennuteetunneliluuk
+		
+//		NB! Stringide kontrollimiseks kasuta süntaksit: if(string1.equals(string2))
+//		Taibudele: Kõige efektiivsem algoritm ei võrdle Stringe vaid tähti.
+	}
+
+}
---
+++ src/praktikum9/WordsLength.java
@@ -0,0 +1,19 @@
+package praktikum9;
+
+public class WordsLength {
+	public static void main(String[] args) {
+		
+//		Kirjutada programm, mis küsib kasutaja käest kümme sõna ja trükib välja sõna pikkuse ja sõna enda.
+//		Näiteks:
+//	
+//		4 kala
+//		13 koerakoonlane
+//		6 lammas
+//		8 Voldemar
+//		....
+		
+		
+		
+	}
+
+}
